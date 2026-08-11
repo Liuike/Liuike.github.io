805ca750b95633b5bca379565c0511d742423cc2
Liuike <lukezhangenyan@gmail.com>
2026-08-11 14:47:56 -0400

    test

--- docs/_plugins/ruby40_compat.rb
+++ docs/_plugins/ruby40_compat.rb
@@ -1,5 +1,8 @@
 # Compatibility shim for older Jekyll/Liquid on modern Ruby.
 # Ruby 3.2+ removed taint support, but Liquid 4 still calls these methods.
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
 module Ruby40TaintCompat
   def tainted?
     false
